2c8847d84d9afc779ee403bb0100e3c52759a688
unknown <i228223@nu.edu.pk>
2025-10-19 23:29:35 +0500

    changed defense.py to set env vars

--- Code/defense_training.py
+++ Code/defense_training.py
@@ -16,6 +16,8 @@ PHASE-1 USE:
 """
 
 import os
+os.environ["SPEECHBRAIN_CACHE_STRATEGY"] = "copy"
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 import math
 import random
 import numpy as np
@@ -108,8 +110,8 @@ config = {
     "lambda_reg": 0.1,
     "device": "cuda" if torch.cuda.is_available() else "cpu",
     "save_dir": "checkpoints_phase1",
-    "train_manifest": "data/librispeech_prepared/train-clean-100_manifest.txt",
-    "val_manifest": "data/librispeech_prepared/dev-clean_manifest.txt"
+    "train_manifest": "../data/librispeech_prepared/train-clean-100_manifest.txt",
+    "val_manifest": "../data/librispeech_prepared/dev-clean_manifest.txt"
 }
 os.makedirs(config["save_dir"], exist_ok=True)
 

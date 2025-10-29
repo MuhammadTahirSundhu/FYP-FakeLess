731bc71ba2a1153fff61417a9c9af8bed7b8fff7
unknown <i228223@nu.edu.pk>
2025-10-30 01:35:06 +0500

    updated app

--- Code/mobile_app_fakeless/lib/Audio_page.dart
+++ Code/mobile_app_fakeless/lib/Audio_page.dart
@@ -1,18 +1,135 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class AudioPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:record/record.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
+
+class AudioPage extends StatefulWidget {
   const AudioPage({super.key});
+
+  @override
+  AudioPageState createState() => AudioPageState();
+}
+
+class AudioPageState extends State<AudioPage> {
+  final AudioRecorder _audioRecorder = AudioRecorder();
+  final AudioPlayer _audioPlayer = AudioPlayer();
+  bool isRecording = false;
+  bool isPlaying = false;
+  String? recordingPath;
+  
+  @override
+  void initState() {
+    super.initState();
+
+    // Listen for when the audio finishes playing
+    _audioPlayer.playerStateStream.listen((state) {
+      if (state.processingState == ProcessingState.completed) {
+        if(!mounted) return;
+        setState(() {
+          isPlaying = false;
+        });
+      }
+    });
+  }
+
+
+  Widget _recordButton(){
+    return FloatingActionButton(onPressed: () async {
+        if(isRecording){
+          String? filePath = await _audioRecorder.stop();
+          if (filePath != null){
+          if(!mounted) return;
+            setState(() {
+              isRecording = false;
+              recordingPath = filePath;
+              });
+        }
+        }else{
+          if (await _audioRecorder.hasPermission()) {
+            final Directory appDocDir = await getApplicationDocumentsDirectory();
+            final String filePath = p.join(appDocDir.path, 'audio.wav');//${DateTime.now().millisecondsSinceEpoch}
+            await _audioRecorder.start(
+              RecordConfig(), 
+              path: filePath);
+              if(!mounted) return;
+              setState(() {
+                isRecording = true;
+                recordingPath = null;
+                });
+          }
+        }
+      },
+      heroTag: 'recordButton', 
+      child: Icon(isRecording ? Icons.stop : Icons.mic)
+    );
+  }
+
+  Widget _playButton(){
+    if(isPlaying){
+      return FloatingActionButton(onPressed: () async {
+        await _audioPlayer.stop();
+        if(!mounted) return;
+        setState(() {
+          isPlaying = false;
+        });
+      }, 
+      heroTag: 'stopButton',
+      child: const Icon(Icons.stop)
+      );
+    }
+    else{
+      return FloatingActionButton(onPressed: () async {
+        if (recordingPath != null){
+          await _audioPlayer.setFilePath(recordingPath!);
+          _audioPlayer.play();
+          if(!mounted) return;
+          setState(() {
+            isPlaying = true;
+          });
+        }
+      }, 
+      heroTag: 'playButton',
+      child: const Icon(Icons.play_arrow)
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Audio Page")),
-      body: const Center(
-        child: Text(
-          "This is the new page!",
-          style: TextStyle(fontSize: 20),
-        ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          spacing: 10,
+          children: <Widget>[
+            recordingPath != null ? _playButton() : Text("No recording available"),
+            _recordButton(),
+            Text(
+              'This is the audio recording page.',
+              style: TextStyle(fontSize: 24),
+            ),
+          ],
+        )
       ),
     );
   }
+
+  @override
+  void dispose() {
+    if (isPlaying) {
+      _audioPlayer.stop();
+    }
+    if (isRecording) {
+      _audioRecorder.stop();
+    }
+    _audioPlayer.dispose();
+    _audioRecorder.dispose();
+    super.dispose();
+  }
+
 }
--- Code/mobile_app_fakeless/lib/main.dart
+++ Code/mobile_app_fakeless/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'Audio_page.dart';
+import 'audio_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -55,19 +55,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   String message = "";
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   void _uploadAudio() {
     // Implement your audio upload logic here
@@ -85,7 +73,6 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       message = "Audio recorded successfully!";
     });
-    print("Record Audio button pressed");
   }
 
   @override
@@ -172,12 +159,12 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        heroTag: 'incrementButton',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   heroTag: 'incrementButton',
+      //   child: const Icon(Icons.add),
+      // ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
